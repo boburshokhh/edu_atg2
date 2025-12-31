efa3d14873893f405efb2649dca9abdd520e7fec
Boburshokh <boburshokhh@example.com>
2025-12-31 16:11:50 +0500

    Update AppLayout component to adjust top padding based on header visibility
    - Changed the top padding class for the main element in AppLayout.vue from 'pt-20' to 'pt-10' when the header is displayed, improving layout consistency.

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -4,7 +4,7 @@
     <Header v-if="showHeader" />
 
     <!-- Main Content -->
-    <main :class="{ 'pt-20': showHeader }">
+    <main :class="{ 'pt-10': showHeader }">
       <slot />
     </main>
 
