4309399ca3123b6de9dc8b45d1ac730f323392fb
Boburshokh <boburshokhh@example.com>
2026-01-04 15:30:56 +0500

    Fix overflow styles in ContentViewer.vue for PDF handling
    - Updated the overflow class for PDF content to ensure proper display and responsiveness, enhancing user experience when viewing PDF files.

--- src/components/lesson/ContentViewer.vue
+++ src/components/lesson/ContentViewer.vue
@@ -3,7 +3,7 @@
     ref="fullscreenContainer"
     :class="[
       'flex-1 flex justify-center bg-[#525659] relative custom-scrollbar',
-      currentFileType === 'pdf' ? 'overflow-hidden h-full' : 'overflow-auto p-8'
+      currentFileType === 'pdf' ? 'overflow-auto h-full' : 'overflow-auto p-8'
     ]"
   >
       <!-- Video Player -->
